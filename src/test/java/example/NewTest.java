4b490909730d38477aab9c7307357c7fb5752a48
Madhu Alishala <malishala@TLP.ads.valuelabs.net>
2018-05-29 17:39:06 +0530

    adding testcases

--- src/test/java/example/NewTest.java
+++ src/test/java/example/NewTest.java
@@ -1,56 +1,44 @@
 package example;
 
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-import org.testng.AssertJUnit;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeTest;
+import static org.testng.Assert.assertEquals;
+
 import org.testng.annotations.Test;
 
 public class NewTest {
 
-	WebDriver driver;
-	ChromeOptions chromeOptions = new ChromeOptions();
+	public int add(int a,int b){
+		return a+b;
+	}
+	
+	/*public int divide(int a,int b){
+		return a/b;
+	}*/
+	
 
+	/*@Test
+	public void contextLoads() {
+		
+	}*/
+	
 	@Test
-	public void testTitle() {
-		driver.get("https://www.valuelabs.com/");
-		System.out.println("Titele verification started");
-		String title = driver.getTitle();
-		//Global software,Product development and IT services company - ValueLabs
-		System.out.println("title "+title);
-		AssertJUnit.assertEquals("Global software,Product development and IT services company - ValueLabs", title);
-
-		/*
-		 * if(driver.getTitle().equals(title)){ System.out.println(
-		 * "Title verification successful"); }else{ System.out.println(
-		 * "title verification fail"); }
-		 */
+	public void getPositiveNumber(){
+		
+		assertEquals(2, add(1,1));
 	}
-
+	
 	@Test
-	public void testUrl() {
-		driver.get("https://www.valuelabs.com/");
-		System.out.println("Url verification started");
-		String url = driver.getCurrentUrl();
-		AssertJUnit.assertEquals(url, "https://www.valuelabs.com/");
+	public void getNegativeNumber(){
+		assertEquals(-2, add(-1,-1));
 	}
-
-	@BeforeTest
-	public void beforeTest() {
-
-		// src/test/resources/org/automation/driver/chromedriver.exe
-		// /usr/bin/chromedriver
-		System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
-		chromeOptions.addArguments("--headless");
-		chromeOptions.addArguments("--no-sandbox");
-		driver = new ChromeDriver(chromeOptions);
+	
+	@Test
+	public void getNegativeResult(){
+		assertEquals(-1, add(-2,1));
 	}
-
-	@AfterTest
-	public void afterTest() {
-		driver.quit();
+	;
+	@Test
+	public void getPositiveResult(){
+		assertEquals(1, add(2,-1));
 	}
 
 }
